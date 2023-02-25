df3f623fcfabe8ec771639e0ffbb7b2031486123
Hanaka Saito <hanaka.saito@uni.minerva.edu>
2023-02-26 07:02:14 +0800

    Update __init__.py

--- frameworkers_main/__init__.py
+++ frameworkers_main/__init__.py
@@ -7,10 +7,9 @@ def create_app():
     # create the app
     app = Flask(__name__)
     # configure the SQLite database, relative to the app instance folder
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///frameworkers.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://jhhsmlfgttdqrd:a03424cbff37ef2d109ae798889b248ce55c2300ac3da0792b4398802f593eb4@ec2-3-211-6-217.compute-1.amazonaws.com:5432/d9ghtq67jeiqsd'
     #"sqlite:///frameworkers.db"
-    #postgresql://frameworkers_user:MBdkxqxBZZuQgYXLhXVMuiGPE16ahw04@dpg-cft605pa6gdotcdifh7g-a.oregon-postgres.render.com/frameworkers
-    # initialize the app with the extension
+    #postgres://jhhsmlfgttdqrd:a03424cbff37ef2d109ae798889b248ce55c2300ac3da0792b4398802f593eb4@ec2-3-211-6-217.compute-1.amazonaws.com:5432/d9ghtq67jeiqsd    # initialize the app with the extension
     app.register_blueprint(main)
     app.debug = False
     db.init_app(app)
