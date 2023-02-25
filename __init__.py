6d9e76a1e86df19670dddca12169d3d51a7e2728
Hanaka Saito <hanaka.saito@uni.minerva.edu>
2023-02-25 15:02:51 +0800

    Add features
    The following changes have been made:
    - Fixed bugs that previously existed in the question feature
    - Modified html/css templates
    - Added bootstrap components into the templates
    - Added data to the database

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# create the extension
 db = SQLAlchemy()
 
 def create_app():
     # create the app
     app = Flask(__name__)
     # configure the SQLite database, relative to the app instance folder
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///frameworkers.db"
+    # create the extension
     # initialize the app with the extension
     db.init_app(app)
-
+    
     return app
