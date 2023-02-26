75aaffd307a798cf3b43a81aed6314bf0c9e382b
Hanaka Saito <hanaka.saito@uni.minerva.edu>
2023-02-26 15:06:20 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -5,18 +5,15 @@ from models import db, Question, Option, Framework,insert_questions, insert_opti
 
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://jhhsmlfgttdqrd:a03424cbff37ef2d109ae798889b248ce55c2300ac3da0792b4398802f593eb4@ec2-3-211-6-217.compute-1.amazonaws.com:5432/d9ghtq67jeiqsd'
-#"sqlite:///frameworkers.db"
-#postgres://jhhsmlfgttdqrd:a03424cbff37ef2d109ae798889b248ce55c2300ac3da0792b4398802f593eb4@ec2-3-211-6-217.compute-1.amazonaws.com:5432/d9ghtq67jeiqsd    # initialize the app with the extension
-app.debug = False
-db.init_app(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///frameworkers.db"
+# db.init_app(app)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    insert_questions()
-    insert_options()
-    insert_frameworks()
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
+#     insert_questions()
+#     insert_options()
+#     insert_frameworks()
 
 #Create a route decorator 
 @app.route('/', methods=['GET', 'POST'])
